7b84345a302316295739879e9fc67afaa4fe5d3c
hwchoi <hwchoi8633@gmail.com>
2023-03-18 09:35:35 +0900

    lsp before

--- src/lsp/PokemonDemo.java
+++ src/lsp/PokemonDemo.java
@@ -1,10 +1,38 @@
 package lsp;
 
-public class Pokemon {
+class Pokemon {
+    public void attack() {
+        System.out.println("공격 실패!");
+    }
+}
+
+class FirePokemon extends Pokemon {
+    @Override
+    public void attack() {
+        System.out.println("불 속성 공격 성공");
+    }
+}
+
+class WaterPokemon extends Pokemon {
+    // 미구현
+}
+
+class ElectricPokemon extends Pokemon {
+    @Override
+    public void attack() {
+        throw new UnsupportedOperationException("전기 공격이 아직 완성되지 않았습니다");
+    }
 }
 
 class Trainer {
     public void attackPokemon(Pokemon pokemon) {
         System.out.println();
+    }
+}
+
+public class PokemonDemo {
+    public static void main(String[] args) {
+        Trainer trainer = new Trainer();
+
     }
 }
