d9e7418e14cb628f9b087adca2ae165963c5e880
hwchoi-nhn <hyunwook.choi@nhnenter.com>
2023-04-15 11:14:14 +0900

    DIP Pattern after

--- src/week7/PokemonGame.java
+++ src/week7/PokemonGame.java
@@ -1,20 +1,26 @@
 package week7;
 
-class Pokemon {
+// after
+
+interface Battleable {
+    void attack();
+}
+
+class Pokemon implements Battleable {
     public void attack() {
         System.out.println("공격!");
     }
 }
 
 class Trainer {
-    private final Pokemon pokemon;
+    private final Battleable battleable;
 
-    public Trainer() {
-        pokemon = new Pokemon();
+    public Trainer(Battleable battleable) {
+        this.battleable = battleable;
     }
 
     public void battle() {
-        pokemon.attack();
+        battleable.attack();
     }
 
     public void catchPokemon() {
@@ -24,8 +30,40 @@ class Trainer {
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Trainer jiwoo = new Trainer();
+        Pokemon pikachu = new Pokemon();
+        Trainer jiwoo = new Trainer(pikachu);
         jiwoo.catchPokemon();
         jiwoo.battle();
     }
 }
+
+// before
+//class Pokemon {
+//    public void attack() {
+//        System.out.println("공격!");
+//    }
+//}
+//
+//class Trainer {
+//    private final Pokemon pokemon;
+//
+//    public Trainer() {
+//        pokemon = new Pokemon();
+//    }
+//
+//    public void battle() {
+//        pokemon.attack();
+//    }
+//
+//    public void catchPokemon() {
+//        System.out.println("포켓몬 획득~");
+//    }
+//}
+//
+//public class PokemonGame {
+//    public static void main(String[] args) {
+//        Trainer jiwoo = new Trainer();
+//        jiwoo.catchPokemon();
+//        jiwoo.battle();
+//    }
+//}
